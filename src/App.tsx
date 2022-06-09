1d8226ec16c5e79b489021f985f0e146af6ebe0c
Smbat Poghosyan <spoghosyan@universe.dart.spb@spoghosyan.nb.ipa.dataart.net>
2022-06-09 19:00:31 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef, useState} from 'react';
+import {createQR} from "@solana/pay";
+import QRCodeStyling from '@solana/qr-code-styling';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: React.FC = () => {
+  const beUrl = process.env.REACT_APP_BE_URL || 'http://localhost:8080';
+  const [qr, setQr]= useState<QRCodeStyling | null>(null);
+
+  console.log(beUrl)
+  const divRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    const initialQr = createQR(beUrl, 256)
+    setQr(initialQr)
+  }, [])
+
+  useEffect(() => {
+    if(qr && divRef.current) {
+      qr.append(divRef.current)
+    }
+
+  }, [qr])
+  return (<div ref={divRef} />);
 }
 
 export default App;
