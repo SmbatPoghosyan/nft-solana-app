230a3d5c00ef6fd96e62153e40660df8e62db8ca
space-d-n <39944650+space-d-n@users.noreply.github.com>
2022-06-10 16:42:32 +0400

    change url to heroku server

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import {createQR} from "@solana/pay";
 import QRCodeStyling from '@solana/qr-code-styling';
 
 const App: React.FC = () => {
-  const beUrl = process.env.REACT_APP_BE_URL || 'http://localhost:8080';
+  const beUrl = process.env.REACT_APP_BE_URL || 'solana:https://solana-nft-server.herokuapp.com/donate' +
+    '?recipient=BHmPGsD73MtK1XzFwNTuFYXoSsTcig7HWbwbq9MufBLv' +
+    '&amount=0.1' +
+    '&message=send SOL to alex wallet';
+  const encodedUrl = encodeURI(beUrl)
   const [qr, setQr]= useState<QRCodeStyling | null>(null);
 
-  console.log(beUrl)
+  console.log(encodedUrl)
   const divRef = useRef<HTMLDivElement>(null)
   useEffect(() => {
-    const initialQr = createQR(beUrl, 256)
+    const initialQr = createQR(encodedUrl, 256)
     setQr(initialQr)
   }, [])
 
