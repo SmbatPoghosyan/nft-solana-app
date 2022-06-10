ec8bdf15c6fe642b74fcbe427debe8aaaeeba96e
zakharyank <karenz@boomte.ch>
2022-06-10 17:44:08 +0400

    fixed url

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,15 @@ import QRCodeStyling from '@solana/qr-code-styling';
 
 const App: React.FC = () => {
   const beUrl = process.env.REACT_APP_BE_URL || 'https://solana-nft-server.herokuapp.com/donate';
-  const recipient = encodeURIComponent('?recipient=BHmPGsD73MtK1XzFwNTuFYXoSsTcig7HWbwbq9MufBLv');
-  const amount = encodeURIComponent('&amount=0.1');
-  const message = encodeURIComponent('&message=send');
+  const recipient = encodeURIComponent('recipient=BHmPGsD73MtK1XzFwNTuFYXoSsTcig7HWbwbq9MufBLv');
+  const amount = encodeURIComponent('amount=0.1');
+  const message = encodeURIComponent('message=send');
 
   const [qr, setQr]= useState<QRCodeStyling | null>(null);
 
   const divRef = useRef<HTMLDivElement>(null)
   useEffect(() => {
-    const fullURL = 'solana:' + beUrl + recipient + amount + message;
+    const fullURL = 'solana:' + beUrl + '?' + recipient + '&' + amount + '&' + message;
     console.log(fullURL)
     const initialQr = createQR(fullURL, 256)
     setQr(initialQr)
