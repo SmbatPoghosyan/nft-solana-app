738655c40d4cf6cec07073bf4284df27518fee63
space-d-n <39944650+space-d-n@users.noreply.github.com>
2022-06-13 15:34:34 +0400

    creating transaction request qr code (v2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,36 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {createQR} from "@solana/pay";
-import QRCodeStyling from '@solana/qr-code-styling';
+import React, {useEffect, useRef} from 'react';
+import {createQR, encodeURL, TransactionRequestURLFields} from "@solana/pay";
 
 const App: React.FC = () => {
-  const beUrl = process.env.REACT_APP_BE_URL || 'https://solana-nft-server.herokuapp.com/donate';
-  const recipient = encodeURIComponent('recipient=BHmPGsD73MtK1XzFwNTuFYXoSsTcig7HWbwbq9MufBLv');
-  const amount = encodeURIComponent('amount=0.1');
-  const message = encodeURIComponent('message=send');
 
-  const [qr, setQr]= useState<QRCodeStyling | null>(null);
+  const qrRef = useRef<HTMLDivElement>(null)
 
-  const divRef = useRef<HTMLDivElement>(null)
+  // Unique address that we can listen for payments to
+  // const reference = useMemo(() => Keypair.generate().publicKey, [])
+
+  const recipient = 'BHmPGsD73MtK1XzFwNTuFYXoSsTcig7HWbwbq9MufBLv'
+  const amount = '0.1'
+
+  // Show the QR code
   useEffect(() => {
-    const fullURL = 'solana:' + beUrl + '?' + recipient + '&' + amount + '&' + message;
-    console.log(fullURL)
-    const initialQr = createQR(fullURL, 256)
-    setQr(initialQr)
-  }, [])
-
-  useEffect(() => {
-    if(qr && divRef.current) {
-      qr.append(divRef.current)
+    // window.location is only available in the browser, so create the URL in here
+    const apiUrl = `https://f75d-83-139-4-54.eu.ngrok.io/donate?recipient=${recipient}&amount=${amount}`
+    const urlParams: TransactionRequestURLFields = {
+      link: new URL(apiUrl),
+      label: "Cookies Inc",
+      message: "Thanks for your order! 🍪",
     }
+    const solanaUrl = encodeURL(urlParams)
+    console.log(`solanaUrl ${solanaUrl}`)
+    const qr = createQR(solanaUrl, 512, 'transparent')
+    if (qrRef.current) {
+      qrRef.current.innerHTML = ''
+      qr.append(qrRef.current)
+    }
+  })
 
-  }, [qr])
-  return (<div ref={divRef} />);
+
+  return (<div ref={qrRef}/>);
 }
 
 export default App;
