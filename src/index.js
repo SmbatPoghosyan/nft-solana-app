271b1067b9d3c433195e8a33553c0b6429f2f08c
zakharyank <karenz@boomte.ch>
2022-06-15 11:26:40 +0400

    js version

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { clusterApiUrl } from '@solana/web3.js';
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
+
+// Default styles that can be overridden by your app
+import '@solana/wallet-adapter-react-ui/styles.css';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+// The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+const network = WalletAdapterNetwork.Devnet;
+
+// You can also provide a custom RPC endpoint.
+const endpoint = clusterApiUrl(network);
+
+// @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+// Only the wallets you configure here will be compiled into your application, and only the dependencies
+// of wallets that your users connect to will be loaded.
+const wallets = [
+    new PhantomWalletAdapter(),
+    new SolflareWalletAdapter({ network }),
+];
+
+
+root.render(
+    <React.StrictMode>
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>
+                    <App />
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
